3f196ac84d9d81a58091714a7a1a41ce3337e8a7
aumdave1979 <aumdave1979@gmail.com>
2025-07-25 11:52:00 +0530

    understanding sqlite data base

--- python/clg_practice.py
+++ python/clg_practice.py
@@ -1,3 +1,14 @@
-import numpy as np 
-arr = np.array([[1,2,3,4,5,6,7,8],[9,10,11,0,0,0,0,0]])
-print(arr)
+import sqlite3
+
+#creating the data base
+conn = sqlite3.connect('Ek1_batch.db')
+#creating a cursor object 
+cursor = conn.cursor()
+#creating student table
+cursor.execute('''CREATE TABLE IF NOT EXISTS student_record (
+                Enrollment INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                Subject TEXT NOT NULL,
+                Mark INTEGER NOT NULL)''')
+#commit changes
+conn.commit()
